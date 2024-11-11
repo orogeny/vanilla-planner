3fdb0b6cfd95832bb361b16e4e25726effe2fdb2
Paul <paulwharin@gmail.com>
2024-11-11 00:44:59 +0000

    feat: enable canvas as drop target

--- src/board.ts
+++ src/board.ts
@@ -1,10 +1,9 @@
-function setup() {
-  const canvas = document.querySelector<HTMLCanvasElement>("canvas.board");
+import { track_catalog } from "./track_catalog";
 
-  if (!canvas) {
-    console.log("canvas not there yet");
-    return;
-  }
+let canvas: HTMLCanvasElement;
+
+function setup() {
+  canvas = document.querySelector<HTMLCanvasElement>("canvas.board")!;
 
   const { clientWidth, clientHeight } = canvas.parentElement!;
 
@@ -13,6 +12,32 @@ function setup() {
 
   canvas.width = clientWidth;
   canvas.height = clientHeight;
+
+  canvas.ondragover = handleDragOver;
+  canvas.ondrop = handleDrop;
+}
+
+// Drag n Drop Event handlers
+
+function handleDragOver(ev: DragEvent) {
+  ev.preventDefault();
+
+  ev.dataTransfer!.dropEffect = "copy";
+}
+
+function handleDrop(ev: DragEvent) {
+  const dropped = ev.dataTransfer!.getData("text/plain");
+
+  const trackId = dropped?.split("#")?.[1] ?? "0";
+
+  const track = track_catalog.find((t) => t.id === trackId);
+
+  if (!track) {
+    console.log(`board::unable to identify dropped item: "${dropped}"`);
+    return;
+  }
+
+  console.log(`successfully dropped: ${track.catno} - ${track.label}`);
 }
 
 export { setup };
