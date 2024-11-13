29dc95d37180a4ae701c40740866fb4b27513e5c
Paul <paulwharin@gmail.com>
2024-11-13 18:57:48 +0000

    refactor: rename Track type 'TrackSpec' to represent the track's specification

--- src/track_catalog.ts
+++ src/track_catalog.ts
@@ -1,4 +1,4 @@
-type Track = {
+type TrackSpec = {
   id: string;
   catno: string;
   label: string;
@@ -9,9 +9,9 @@ type Track = {
 const SLEEPER_LENGTH = 22;
 const TRACK_GUAGE = 12;
 
-const track_catalog: Track[] = [
+const track_catalog: TrackSpec[] = [
   { id: "1", catno: "TT8002", label: "166mm", colour: "#0bff01", length: 166 },
   { id: "2", catno: "TT8039", label: "332mm", colour: "#fe00f6", length: 322 },
 ];
 
-export { SLEEPER_LENGTH, TRACK_GUAGE, track_catalog, type Track };
+export { SLEEPER_LENGTH, TRACK_GUAGE, track_catalog, type TrackSpec };
